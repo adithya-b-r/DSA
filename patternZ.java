057bef67418d64d02d38a627485f00db5ea93bd2
Abr009 <bradithya945@gmail.com>
2024-09-24 17:57:30 +0530

    Initial commit

---
+++ patternZ.java
@@ -0,0 +1,18 @@
+class patternZ {
+
+  static void pattern(int n) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 0; j < n; j++) {
+        if (i == 1 || i == n || j == n-i)
+          System.out.print("*");
+        else
+          System.out.print(" ");
+      }
+      System.out.println();
+    }
+  }
+
+  public static void main(String[] args) {
+    pattern(4);
+  }
+}
