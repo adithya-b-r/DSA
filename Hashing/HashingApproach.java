7eee3b2defcd4e8ae1d9fe886a8f658f97063218
Abr009 <bradithya945@gmail.com>
2024-10-04 09:42:48 +0530

    Updated files

--- Hashing/HashingApproach.java
+++ Hashing/HashingApproach.java
@@ -1,7 +1,7 @@
 // package Hashing;
 import java.util.Scanner;
 
-class HashingApproach{
+class hashingApproach{
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
