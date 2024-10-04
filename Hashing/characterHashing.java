6d444aa38a630df6ea9b6ce3cd6011bbcfa7150a
Abr009 <bradithya945@gmail.com>
2024-10-04 09:49:30 +0530

    Updated files

---
+++ Hashing/characterHashing.java
@@ -0,0 +1,33 @@
+// package Hashing;
+import java.util.Scanner;
+
+class characterHashing{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter size of array : ");
+    int n = sc.nextInt();
+
+    char arr[] = new char[n];
+
+    System.out.print("Enter elements of array : ");
+    for(int i=0; i<n; i++)
+      arr[i] = sc.next().charAt(0);
+
+    int hash[] = new int[256];
+    for(int i=0; i<n; i++)
+      hash[arr[i]]++;
+
+    System.out.print("Enter value for q : ");
+    int q = sc.nextInt();
+
+    while(q-- > 0){
+      char ch;
+      System.out.print("Enter a character : ");
+      ch = sc.next().charAt(0);
+      System.out.println("Count of "+ch+" : "+hash[ch]);
+    }
+
+    sc.close();
+  }
+}
