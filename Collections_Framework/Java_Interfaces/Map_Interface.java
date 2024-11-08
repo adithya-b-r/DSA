e79a4129a0011ad2e95c80832e80bbe60cbdf897
Abr009 <bradithya945@gmail.com>
2024-11-08 21:11:17 +0530

    Added Set Interface

--- Collections_Framework/Java_Interfaces/Map_Interface.java
+++ Collections_Framework/Java_Interfaces/Map_Interface.java
@@ -1,5 +1,17 @@
 package Collections_Framework.Java_Interfaces;
+import java.util.*;
 
 public class Map_Interface {
-  
+  public static void main(String[] args) {
+    Map<String, Integer> fruits = new HashMap<>();
+
+    // Add key-value pairs to the Map
+    fruits.put("Apple", 10);
+    fruits.put("Banana", 5);
+    fruits.put("Orange", 8);
+
+    // Retrieve the value using key
+    System.out.println("Apple count : "+ fruits.get("Apple"));
+    
+  }
 }
