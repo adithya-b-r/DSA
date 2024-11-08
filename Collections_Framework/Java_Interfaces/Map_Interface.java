1f31ad4c06610b1b37ee8d32aa1ca806bdc35c5b
Abr009 <bradithya945@gmail.com>
2024-11-08 21:17:38 +0530

    Added Map Interface

--- Collections_Framework/Java_Interfaces/Map_Interface.java
+++ Collections_Framework/Java_Interfaces/Map_Interface.java
@@ -1,4 +1,5 @@
 package Collections_Framework.Java_Interfaces;
+
 import java.util.*;
 
 public class Map_Interface {
@@ -10,8 +11,29 @@ public class Map_Interface {
     fruits.put("Banana", 5);
     fruits.put("Orange", 8);
 
+    // Display Map
+    System.out.println("Fruits : " + fruits);
+
     // Retrieve the value using key
-    System.out.println("Apple count : "+ fruits.get("Apple"));
-    
+    System.out.println("Apple count : " + fruits.get("Apple"));
+
+    // Check if Map contains certain value
+    System.out.println("Contains count 8 : " + fruits.containsValue(8));
+
+    // Check if Map contains certain key
+    System.out.println("Contains orange : " + fruits.containsKey("Orange"));
+
+    // Remove a key-value pair
+    fruits.remove("Orange");
+    System.out.println(fruits);
+
+    // Get the size of the Map
+    System.out.println("Map size : " + fruits.size());
+
+    // Get all keys
+    System.out.println("Keys : "+ fruits.keySet());
+
+    // Get all values
+    System.out.println("Values : "+ fruits.values());
   }
 }
