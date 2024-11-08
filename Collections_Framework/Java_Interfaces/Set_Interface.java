eddea7fcd8e5edfc821352ea1dedf0bb765fb227
Abr009 <bradithya945@gmail.com>
2024-11-08 21:06:29 +0530

    Added Set Interface

--- Collections_Framework/Java_Interfaces/Set_Interface.java
+++ Collections_Framework/Java_Interfaces/Set_Interface.java
@@ -1,5 +1,27 @@
 package Collections_Framework.Java_Interfaces;
+import java.util.*;
 
 public class Set_Interface {
-  
+  public static void main(String[] args) {
+    Set<String> uniqueItems = new HashSet<>();
+
+    // Add elements to the set
+    uniqueItems.add("C");
+    uniqueItems.add("A");
+    uniqueItems.add("B");
+    uniqueItems.add("A"); //Duplicate won't be added
+
+    // Display the set
+    System.out.println(uniqueItems);
+
+    // Check if an item exits
+    System.out.println("Contains 'A' ? "+ uniqueItems.contains("A"));
+
+    // Remove an element 
+    uniqueItems.remove("B");
+    System.out.println("After removing an element : "+ uniqueItems);
+
+    // Check Set size
+    System.out.println("Set size : "+ uniqueItems.size());
+  }
 }
