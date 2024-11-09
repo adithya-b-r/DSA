5b45067b9db4417bfe1ca8fcfdc44848002e5658
Abr009 <bradithya945@gmail.com>
2024-11-09 12:23:43 +0530

    Added PriorityQueue Class

--- Collections_Framework/Classes_That_Implement_Interfaces/PriorityQueue_Class.java
+++ Collections_Framework/Classes_That_Implement_Interfaces/PriorityQueue_Class.java
@@ -1,3 +1,24 @@
+import java.util.*;
+
 public class PriorityQueue_Class {
-  
+  public static void main(String[] args) {
+    PriorityQueue<Integer> queue = new PriorityQueue<>();
+
+    // Adding elements to queue
+    queue.add(10);
+    queue.offer(5);
+
+    System.out.println("Elements in queue : " + queue);
+
+    System.out.println("Retrives head without removing: " + queue.peek());
+
+    System.out.println("Retrives and removes head : " + queue.poll());
+
+    System.out.println("Queue size : " + queue.size());
+    System.out.println("Is queue empty? : " + queue.isEmpty());
+
+    System.out.println("Elements in queue : " + queue);
+
+    queue.clear();
+  }
 }
