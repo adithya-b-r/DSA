ce580f12430087aa7a48a21931c82bea63760a2d
Abr009 <bradithya945@gmail.com>
2024-11-09 12:01:01 +0530

    Added LinkedList Class

--- Collections_Framework/Classes_That_Implement_Interfaces/HashSet_Class.java
+++ Collections_Framework/Classes_That_Implement_Interfaces/HashSet_Class.java
@@ -1,3 +1,22 @@
+import java.util.*;
+
 public class HashSet_Class {
-  
+  public static void main(String[] args) {
+    HashSet<String> set = new HashSet<>();
+
+    System.out.println("Trying to add 'Apple' to set : "+set.add("Apple"));
+    set.add("Banana");
+    System.out.println("Trying to add 'Apple' for 2nd time to set : "+set.add("Apple"));
+
+    set.remove("Banana");
+    System.out.println("Contain Apple? : " + set.contains("Apple"));
+
+    //Displaying contents of set
+    System.out.println(set);
+
+    System.out.println("Set size : "+set.size());
+
+    System.out.println("Is set empty? : "+set.isEmpty());
+    set.clear();
+  }
 }
--- Collections_Framework/Classes_That_Implement_Interfaces/LinkedList_Class.java
+++ Collections_Framework/Classes_That_Implement_Interfaces/LinkedList_Class.java
@@ -1,3 +1,33 @@
+import java.util.*;
+
 public class LinkedList_Class {
-  
+  public static void main(String[] args) {
+    LinkedList<String> list = new LinkedList<>();
+
+    list.add("Mango"); // Adds to end
+    list.addFirst("Banana"); // Adds at beginning
+    list.addLast("Orange"); // Adds at end
+
+    System.out.println("Return index[0] : " + list.get(0)); // Return element at end
+    System.out.println("Return first element : " + list.getFirst()); // Returns first element
+    System.out.println("Return last element : " + list.getLast()); // Returns last element
+
+    System.out.println("Before emoving : " + list);
+    list.removeFirst(); // Removes first element
+    System.out.println("After removing first element: " + list);
+    list.removeLast(); // Removes last element
+    System.out.println("After removing last element: " + list);
+
+    System.out.println("Retrives and remove head : " + list.poll());
+
+    // Adds at end
+    list.offer("Grapes");
+
+    System.out.println("List size : " + list.size());
+    System.out.println("Contains Apple? : " + list.contains("Apple"));
+    System.out.println("Is list empty? : " + list.isEmpty());
+
+    //Removes all the element
+    list.clear();
+  }
 }
