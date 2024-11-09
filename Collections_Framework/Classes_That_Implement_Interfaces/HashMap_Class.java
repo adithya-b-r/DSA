24cc744989806275ec4ac362e5203745d428ee45
Abr009 <bradithya945@gmail.com>
2024-11-09 12:14:09 +0530

    Added HashMap Class

--- Collections_Framework/Classes_That_Implement_Interfaces/HashMap_Class.java
+++ Collections_Framework/Classes_That_Implement_Interfaces/HashMap_Class.java
@@ -1,3 +1,35 @@
+import java.util.*;
+
 public class HashMap_Class {
-  
+  public static void main(String[] args) {
+    HashMap<String, Integer> map = new HashMap<>();
+
+    // Adding key-value pairs
+    map.put("Apple", 10);
+    map.put("Banana", 5);
+    map.put("Cherry", 8);
+
+    // Display all pairs
+    System.out.println(map);
+
+    // Accessing value of Apple
+    System.out.println("value of Apple : " + map.get("Apple"));
+
+    // Remove entry using key
+    map.remove("Banana");
+
+    System.out.println("Map contains Apple? : " + map.containsKey("Apple"));
+    System.out.println("Map contains Value 8? " + map.containsValue(8));
+
+    System.out.println("Map size : " + map.size());
+
+    System.out.println("Is map empty? : "+ map.isEmpty());
+    
+    System.out.println("All keys : "+map.keySet());
+    System.out.println("All values : "+map.values());
+    System.out.println("Key-value pairs : "+ map.entrySet());
+
+    // Clear map
+    map.clear();
+  }
 }
