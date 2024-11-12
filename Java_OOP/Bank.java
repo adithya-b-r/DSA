c847f14594f1d1b53de0a1d86b28e5be956e2b3e
Abr009 <bradithya945@gmail.com>
2024-11-12 22:05:49 +0530

    Added Encapsulation, Constructer, getter

--- Java_OOP/Bank.java
+++ Java_OOP/Bank.java
@@ -1,13 +1,13 @@
-public class Bank{
-  //Encapsulation
+public class Bank {
+  // Encapsulation
   private String accountHolder;
   private long accountNumber;
   private double balance;
   private boolean isActive;
   private int secretPin;
 
-  //Constructor
-  public Bank(String accountHolder, long accountNumber, double balance, boolean isActive, int secretPin){
+  // Constructor
+  public Bank(String accountHolder, long accountNumber, double balance, boolean isActive, int secretPin) {
     this.accountHolder = accountHolder;
     this.accountNumber = accountNumber;
     this.balance = balance;
@@ -15,8 +15,63 @@ public class Bank{
     this.secretPin = secretPin;
   }
 
-  public String getAccountHolder(){
+  // Accessing Private values using getter and setter.
+  public String getAccountHolder() {
     return accountHolder;
   }
 
+  public long getAccountNumber() {
+    return accountNumber;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public boolean getIsActive() {
+    return isActive;
+  }
+
+  public int getSecretPin() {
+    return secretPin;
+  }
+
+  public String deposit(long amount) {
+    if (isActive) {
+      if (amount > 0) {
+        balance += amount;
+        return "Deposit successful!";
+      } else {
+        return "Amount must be greater than ₹0";
+      }
+    } else {
+      return "Unable to process the request";
+    }
+  }
+
+  public boolean setIsActive(boolean status) {
+    isActive = status;
+    return status;
+  }
+
+  public String withdraw(long amount, int pin) {
+    if(pin != secretPin){
+      return "Invalid PIN.";
+    }
+
+    if (isActive) {
+      if (amount > 0) {
+        if (amount <= balance - 500) {
+          balance -= amount;
+          return "Withdrew ₹" + amount + " successfully. Current balance: " + balance;
+        } else {
+          return "Insufficient balance";
+        }
+      } else {
+        return "Insufficient balance";
+      }
+    }else{
+      return "Unable to process the request";
+    }
+  }
 }
