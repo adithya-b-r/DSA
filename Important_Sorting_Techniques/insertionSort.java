80a1558c6149c6e51d174063fef5d909ae252ce4
Abr009 <bradithya945@gmail.com>
2024-11-24 13:59:53 +0530

    Added Insertion Sort

---
+++ Important_Sorting_Techniques/insertionSort.java
@@ -0,0 +1,24 @@
+public class insertionSort {
+  public static void main(String args[]) {
+    int arr[] = { 7, 5, 2, 8, 1, 3, 4, 6 };
+    int n = arr.length;
+
+    for (int i = 0; i < n - 1; i++) {
+      int j = i;
+
+      while(j>0 && arr[j-1] > arr[j]){
+        int temp = arr[j-1];
+        arr[j-1] = arr[j];
+        arr[j] = temp;
+
+        j--;
+      }
+    }
+
+    System.out.print("Sorted Array: ");
+    for(int i=0; i<n; i++){
+      System.out.print(arr[i] + " ");
+    }
+
+  }
+}
