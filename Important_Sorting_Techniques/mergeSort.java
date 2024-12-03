a8df046e87432eb9745d4130ab5ab1492d0b7552
Abr009 <bradithya945@gmail.com>
2024-12-03 21:21:11 +0530

    Added MergeSort

--- Important_Sorting_Techniques/mergeSort.java
+++ Important_Sorting_Techniques/mergeSort.java
@@ -1,30 +1,27 @@
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
 
-public class mergeSort {
+class mergeSort {
   static void merge(int arr[], int low, int mid, int high) {
     ArrayList<Integer> temp = new ArrayList<>();
+
     int left = low;
     int right = mid + 1;
 
     while (left <= mid && right <= high) {
       if (arr[left] <= arr[right]) {
-        temp.add(arr[left]);
-        left++;
+        temp.add(arr[left++]);
       } else {
-        temp.add(arr[right]);
-        right++;
+        temp.add(arr[right++]);
       }
     }
 
     while (left <= mid) {
-      temp.add(arr[left]);
-      left++;
+      temp.add(arr[left++]);
     }
 
     while (right <= high) {
-      temp.add(arr[right]);
-      right++;
+      temp.add(arr[right++]);
     }
 
     for (int i = low; i <= high; i++) {
@@ -32,33 +29,36 @@ public class mergeSort {
     }
   }
 
-  static void mergeSorting(int arr[], int low, int high) {
-    if (low >= high)
+  static void mergeSorting(int arr[], int low, int high){
+    if(low >= high)
       return;
 
-    int mid = (low + high) / 2;
+    int mid = (low+high)/2;
     mergeSorting(arr, low, mid);
-    mergeSorting(arr, mid + 1, high);
+    mergeSorting(arr, mid+1, high);
+
     merge(arr, low, mid, high);
   }
 
   public static void main(String args[]) {
-    Scanner sc = new Scanner(System.in);
+    int i, n;
 
+    Scanner sc = new Scanner(System.in);
     System.out.print("Enter size of Array: ");
-    int n = sc.nextInt();
+    n = sc.nextInt();
 
     int arr[] = new int[n];
 
-    for (int i = 0; i < n; i++) {
-      System.out.print("Enter element " + i + ": ");
+    for (i = 0; i < n; i++) {
+      System.out.print("Enter element " + (i + 1) + ": ");
       arr[i] = sc.nextInt();
     }
 
-    mergeSorting(arr, 0, arr.length - 1);
+    mergeSorting(arr, 0, n-1);
 
-    System.out.println("After sorting: ");
-    for (int i = 0; i < n; i++)
+    System.out.println("Array after sorting: ");
+    for (i = 0; i < n; i++) {
       System.out.print(arr[i] + " ");
+    }
   }
 }
