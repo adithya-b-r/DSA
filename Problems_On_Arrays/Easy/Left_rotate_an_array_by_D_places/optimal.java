376c9b4be3f1740675dfb4fed7bdbae12ac1ee41
Abr009 <bradithya945@gmail.com>
2024-12-08 11:26:13 +0530

    [Added] Left Rotate an array by D places

--- Problems_On_Arrays/Easy/Left_rotate_an_array_by_D_places/optimal.java
+++ Problems_On_Arrays/Easy/Left_rotate_an_array_by_D_places/optimal.java
@@ -1,5 +1,21 @@
 package Left_rotate_an_array_by_D_places;
 
 class optimal {
-  
+  public static void main(String[] args) {
+    int arr[] = {1, 2, 3, 4, 5};
+    int k=3;
+    int n = arr.length, temp;
+
+    for(int i=0; i<k; i++){
+      temp = arr[0];
+      for(int j=1; j<n; j++)
+        arr[j-1] = arr[j];
+
+      arr[n-1] = temp;
+    }
+
+    for(int i=0; i<n; i++){
+      System.out.print(arr[i]+" ");
+    }
+  }
 }
