61dae7cc7b6cc6de80de9dc1c08e1b45eeeaf8e1
Abr009 <bradithya945@gmail.com>
2024-12-08 11:50:34 +0530

    [Added] Right Rotate an array by D places

---
+++ Problems_On_Arrays/Easy/Right_Rotate_an_array_by_K_place/optimal.java
@@ -0,0 +1,29 @@
+package Right_Rotate_an_array_by_K_place;
+
+import java.util.Scanner;
+
+class optimal {
+  public static void main(String[] args) {
+    int arr[] = { 1, 2, 3, 4, 5, 6, 7 };
+    int n = arr.length;
+    int temp, k, i, j;
+
+    Scanner sc = new Scanner(System.in);
+
+    System.out.print("Enter value for K: ");
+    k = sc.nextInt();
+
+    for (i = 0; i < k; i++) {
+      temp = arr[n - 1];
+      for (j = n-1; j > 0; j--) {
+        arr[j] = arr[j-1];
+      }
+
+      arr[0] = temp;
+    }
+
+    for (i = 0; i < n; i++) {
+      System.out.print(arr[i] + " ");
+    }
+  }
+}
