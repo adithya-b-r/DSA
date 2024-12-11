01e399679edd5ee29e70edb54d9c4aa548d24796
Abr009 <bradithya945@gmail.com>
2024-12-11 08:13:50 +0530

    Move all Zeros to the end of the array.

--- Problems_On_Arrays/Easy/Move_Zeros_to_End/optimal.java
+++ Problems_On_Arrays/Easy/Move_Zeros_to_End/optimal.java
@@ -8,7 +8,7 @@ class optimal {
   }
 
   public static void main(String[] args) {
-    int arr[] = { 1, 0, 2, 3, 2, 0, 0, 4, 5, 1 };
+    int arr[] = { 1, 0, 2, 3, 2, 0, 0, 4, 5, 1, 0 };
     int i, j = 0;
 
     for (i = 0; i < arr.length; i++) {
