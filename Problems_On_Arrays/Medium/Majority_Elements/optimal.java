98befe4055ce27a50fc289d65fd8be68a0ef5181
Abr009 <bradithya945@gmail.com>
2025-01-05 19:30:29 +0530

    Added Majority element

--- Problems_On_Arrays/Medium/Majority_Elements/optimal.java
+++ Problems_On_Arrays/Medium/Majority_Elements/optimal.java
@@ -1,3 +1,29 @@
+// Moore's Voting Algorithm
+
 class optimal {
-  
+  public static void main(String[] args) {
+    int arr[] = { 2, 2, 3, 3, 1, 3, 2 };
+    int n = arr.length;
+    int ele = -1, count = 0;
+
+    for (int num : arr) {
+      if (count == 0)
+        ele = num;
+
+      if (num == ele)
+        count++;
+      else
+        count--;
+    }
+
+    count = 0;
+    for (int num : arr)
+      if (ele == num)
+        count++;
+
+    if (count > (n / 2))
+      System.out.println("Majority Element: " + ele);
+    else
+      System.out.println("Majority Element: " + -1);
+  }
 }
