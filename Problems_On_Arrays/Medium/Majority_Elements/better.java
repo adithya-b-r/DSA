3bedcfbf2d3a6ff5e713c117c47bf06a14b0ef78
Abr009 <bradithya945@gmail.com>
2025-01-05 14:17:22 +0530

    Added Majority element

--- Problems_On_Arrays/Medium/Majority_Elements/better.java
+++ Problems_On_Arrays/Medium/Majority_Elements/better.java
@@ -5,19 +5,23 @@ class better {
     int arr[] = { 2, 2, 3, 3, 1, 2, 2 };
     int n = arr.length;
 
+    int majority = -1;
+
     HashMap<Integer, Integer> map = new HashMap<>();
 
     for (int num : arr) {
       if (map.containsKey(num)) {
-        map.put(num, map.get(num)+1);
+        map.put(num, map.get(num) + 1);
       } else {
         map.put(num, 1);
       }
     }
 
-    for(Map.Entry<Integer, Integer> it : map.entrySet()){
-      if(it.getValue() > (n/2))
-        System.out.println("Majority Element: "+ it.getKey());
+    for (Map.Entry<Integer, Integer> it : map.entrySet()) {
+      if (it.getValue() > (n / 2))
+        majority = it.getKey();
     }
+
+    System.out.println("Majority Element: " + majority);
   }
 }
